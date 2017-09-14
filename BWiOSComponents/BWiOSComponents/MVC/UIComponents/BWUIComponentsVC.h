524b6c19cda7ec309d8a60fcd7b7b17fb29b7468
BobWong <wangbo@bluemoon.com.cn>
2017-09-14 16:52:41 +0800

    Modify

---
+++ BWiOSComponents/BWiOSComponents/MVC/UIComponents/BWUIComponentsVC.h
@@ -0,0 +1,13 @@
+//
+//  BWUIComponentsVC.h
+//  BWiOSComponents
+//
+//  Created by BobWong on 2017/9/14.
+//  Copyright © 2017年 BobWongStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BWUIComponentsVC : UIViewController
+
+@end
---
+++ BWiOSComponents/BWiOSComponents/MVC/VerifyCodeManager/BWVerifyCodeManagerVC.h
@@ -0,0 +1,13 @@
+//
+//  BWVerifyCodeManagerVC.h
+//  BWiOSComponents
+//
+//  Created by BobWong on 2017/9/14.
+//  Copyright © 2017年 BobWongStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BWVerifyCodeManagerVC : UIViewController
+
+@end
