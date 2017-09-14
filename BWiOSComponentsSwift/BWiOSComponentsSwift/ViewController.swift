0f7212e600fba01b5bfd5a744304115955930f7e
BobWong <wangbo@bluemoon.com.cn>
2017-09-14 14:01:02 +0800

    Modify

---
+++ BWiOSComponentsSwift/BWiOSComponentsSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BWiOSComponentsSwift
+//
+//  Created by BobWong on 2017/9/14.
+//  Copyright © 2017年 BobWongStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
